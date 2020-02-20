d2bbc0014888c66bb0cc05cf92223081383833cb
pailinglo <pailinglo@gmail.com>
2020-02-20 08:54:17 -0500

    121. Change password in asp.net core

--- HelloCoreApp/Controllers/AccountController.cs
+++ HelloCoreApp/Controllers/AccountController.cs
@@ -431,5 +431,52 @@ namespace HelloCoreApp.Controllers
             // Display validation errors if model state is not valid
             return View(model);
         }
+
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //get the current user
+                var user = await userManager.GetUserAsync(User);
+
+                if (user == null)
+                {
+                    RedirectToAction("Login");
+                }
+
+                var result = await userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+                
+                // The new password did not meet the complexity rules or
+                // the current password is incorrect. Add these errors to
+                // the ModelState and rerender ChangePassword view
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+                
+                // Upon successfully changing the password refresh sign-in cookie
+                await signInManager.RefreshSignInAsync(user);
+                return View("ChangePasswordConfirmation");
+
+            }
+
+
+            return View(model);
+        }
+
+
+
     }
 }
---
+++ HelloCoreApp/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloCoreApp.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Display(Name ="Current Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+        
+        [Required]
+        [Display(Name ="New Password")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Compare("NewPassword",ErrorMessage = "The new password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
